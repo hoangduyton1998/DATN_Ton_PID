a9d7da602d1dab8e3d633bf73e89727e131a0bd0
hoangduyton1998 <49790278+hoangduyton1998@users.noreply.github.com>
2021-04-09 21:10:35 +0700

    Update pid_example.cpp

--- pid_example.cpp
+++ pid_example.cpp
@@ -4,13 +4,13 @@
 
 int main() {
 
-    PID pid = PID(0.01, 100, -100, 1, 0.001, 0.5);
+    PID pid = PID(0.01, 100, -100, 1, 0.001, 0.5); // dat gia tri (delta t, max, min, Kp, Kd, Ki)
 
-    double val = 50;
+    double val = 50; // Gia tri ban dau
     for (int i = 0; i < 200; i++) {
-        double inc = pid.calculate(0, val);
+        double inc = pid.calculate(0, val); // Tinh toan de dua ra output, dau vao la (gia tri can set, gia tri hien tai)
         std::cout << "val:" << val << "\n inc: " << inc << "\n \n";
-        val += inc;
+        val += inc; // Cap nhat output
     }
 
     return 0;
